defb7574830f62df67fc3aa00731b55c5e70f9df
rayray <r6deng@uwaterloo.ca>
2017-01-22 21:10:10 -0500

    added simple jquery and javascript anamation effect

---
+++ script.js
@@ -0,0 +1,13 @@
+
+
+$(document).ready(function() {
+    setInterval ('cursorAnimation()', 820);
+});
+
+function cursorAnimation() {
+	$('#cursor').animate({
+	    opacity: 0
+	}, 'fast', 'swing').animate({
+	    opacity: 1
+	}, 'fast', 'swing');
+}
