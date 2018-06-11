daaa48b82515a8c1fec87dce3ec1abb30bc01e67
Raymond <r6deng@uwaterloo.ca>
2018-06-11 18:59:37 -0400

    added erase and type effect

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 $(document).ready(function() {
-    setInterval ('cursorAnimation()', 820);
+    typingEffect();
 });
 
 function cursorAnimation() {
@@ -10,3 +10,45 @@ function cursorAnimation() {
 	    opacity: 1
 	}, 'fast', 'swing');
 }
+
+const words = ["Raymond", "Data Scientist", "Developer"];
+let i = 0;
+let timer;
+
+function typingEffect() {
+    let word = words[i].split("");
+    var loopTyping = function() {
+        if (word.length > 0) {
+            document.getElementById('word').innerHTML += word.shift();
+        } else {
+            clearTimeout(timer);
+            deletingEffect();
+            return false;
+        };
+        timer = setTimeout(loopTyping, 300);
+    };
+    loopTyping();
+};
+
+function deletingEffect() {
+    let word = words[i].split("");
+    var loopDeleting = function() {
+        if (word.length > 0) {
+            word.pop();
+            document.getElementById('word').innerHTML = word.join("");
+        } else {
+            if (words.length > (i + 1)) {
+                i++;
+            } else {
+                i = 0;
+            };
+            clearTimeout(timer);
+            typingEffect();
+            return false;
+        };
+        timer = setTimeout(loopDeleting, 100);
+    };
+    loopDeleting();
+};
+
+typingEffect();
